56f6b710ec8e11e9a5f18d3d53cb4c8b7c008718
Andrew Dant <andrew.dant@scatech.com>
2019-05-18 12:35:19 -0400

    Gross ROI table working + basic styling

--- app/controllers/cc-roi.js
+++ app/controllers/cc-roi.js
@@ -1,6 +1,27 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+	NUM_WEEKS: 4,
+
 	catValue: 60,
-	systemCost: 6950
+	systemCost: 6950,
+	rowList: computed('catValue', 'systemCost', function() {
+		let NUM_WEEKS = this.get('NUM_WEEKS');
+		let catValue = this.get('catValue');
+		let systemCost = this.get('systemCost');
+		
+		let rowList = [];
+		for(let catsPer=10; catsPer <= 100; catsPer+=10) {
+			let row = [];
+			for(let week=0; week<=NUM_WEEKS; week++) {
+				if (week == 0)
+					row.push(catsPer);
+				else
+					row.push(catValue * catsPer * week - systemCost);
+			}
+			rowList.push(row);
+		}
+		return rowList;
+	})
 });
