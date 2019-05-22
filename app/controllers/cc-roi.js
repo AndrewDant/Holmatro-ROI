3006e5d84ed532bb2f1c7371e828142d7e96b7c4
Andrew Dant <andrew.dant@scatech.com>
2019-05-22 15:32:37 -0400

    added the ROI graph

--- app/controllers/cc-roi.js
+++ app/controllers/cc-roi.js
@@ -1,11 +1,13 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Controller.extend({
 	NUM_WEEKS: 4,
 
 	catValue: 60,
 	systemCost: 6950,
+	graphCatsPer: 50,
 	rowList: computed('catValue', 'systemCost', function() {
 		let NUM_WEEKS = this.get('NUM_WEEKS');
 		let catValue = this.get('catValue');
@@ -23,5 +25,39 @@ export default Controller.extend({
 			rowList.push(row);
 		}
 		return rowList;
-	})
+	}),
+
+	graphData: computed('catValue', 'systemCost', 'graphCatsPer', function() {
+		let NUM_WEEKS = this.get('NUM_WEEKS');
+		let catValue = this.get('catValue');
+		let systemCost = this.get('systemCost');
+		let catsPer = this.get('graphCatsPer');
+
+		let date = moment();
+		let labels = [];
+		let dataPoints = [];
+
+		for(let week=0; week<=NUM_WEEKS; week++) {
+			labels.push(date.format('MM-DD'));
+			date.add(7, 'days');
+			dataPoints.push(catValue * catsPer * week - systemCost);
+		}
+
+		let graphData = {
+			labels: labels,
+			datasets: [{
+				data: dataPoints,
+				backgroundColor: 'rgba(244, 101, 35, .5)',
+	            borderColor: 'rgba(244, 101, 35, 1)',
+	            pointBackgroundColor: 'rgba(244, 101, 35, 1)'
+			}]
+		}
+		return graphData;
+	}),
+
+	graphOptions: {
+		legend: {
+			display: false
+		}
+	}
 });
